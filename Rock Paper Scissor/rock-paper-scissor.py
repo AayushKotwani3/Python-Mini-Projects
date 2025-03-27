fbaacf76859404b1d8f7023340c12df39bcce138
AayushKotwani3 <aayushkotwani9@gmail.com>
2025-03-27 23:54:16 +0530

    Added Comments

--- Rock Paper Scissor/rock-paper-scissor.py
+++ Rock Paper Scissor/rock-paper-scissor.py
@@ -20,20 +20,21 @@ def check(comp,user):   # If both choices are the same
     if (comp==user): # If both choices are the same
         return "Draw"
     elif (comp==1 and user==2) or (comp==2 and user==3) or (comp==3 and user==1):
-        return "Win"
+        return "Win"    # Player wins
     else:
-        return "Lose"
+        return "Lose"   # Computer wins
 
+# Main function to play Rock-Paper-Scissors
 def rps():
     user_counter=0
     computer_counter=0
 
     for i in range(1,rounds+1):
 
-        comp=random.randint(1,3)
+        comp=random.randint(1,3)    # Computer randomly selects Rock, Paper, or Scissors
         print(f"Round {i}")
         print("I chose my value your turn")
-        user=int(input("Type 1 for Rock ,2 for Paper ,3 for scissor\n"))
+        user=int(input("Type 1 for Rock ,2 for Paper ,3 for scissor\n"))    # User input
 
         if check(comp,user)=='Draw':
             user_counter+=0
@@ -48,7 +49,9 @@ def rps():
             computer_counter+=1  
             print(f"Winner of  Round {i} is Computer\n\nComputer chose{choice(comp)}\nYou chose{choice(user)}\n")
     print()        
-    print(f"Final Scores:\n Player:{user_counter}\n Computer:{computer_counter}")        
+    print(f"Final Scores:\n Player:{user_counter}\n Computer:{computer_counter}")  
+
+    # Display final scores and the winner      
     if user_counter==computer_counter:
         print("The Game is draw")
     elif user_counter>computer_counter:
@@ -56,7 +59,7 @@ def rps():
     else:
         print("Winner of this game is Computer")
             
-
+# Game Introduction
 print("Welcome to the game: Rock-Paper-scissor \n")
 name=input("Enter your name to start - ")
 rounds=int(input("Choose the no of rounds You want to play - "))
