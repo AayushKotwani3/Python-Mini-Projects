f1dd6ff11d29db7abcc26917ea2842f06d4c4caa
AayushKotwani3 <aayushkotwani9@gmail.com>
2025-04-16 00:41:25 +0530

    Completed the PIG game

--- PIG/PIG.py
+++ PIG/PIG.py
@@ -16,5 +16,35 @@ while True:
             print("Enter players between 2 and 4 only")
     else:
         print("Invalid Try again")        
-     
-        
+
+max_score=50
+player_score=[0 for _ in range(players)]
+
+while(max(player_score)<max_score):
+    for player_index in range(players):
+        print("\nPlayer,",player_index+1,"Turn has just started")
+        print("Your total score is ",player_score[player_index])
+        current_score=0 
+
+        while True:      
+
+            should_roll=input("Would you like to roll the dice (y):")
+            if should_roll.lower()!='y':
+                break
+
+            value=roll()
+
+            if value==1:
+                print("You rolled 1 Turn done!")
+                current_score=0
+                break
+            else:
+                current_score+=value
+                print("You rolled a ",value)  
+            print("your current score is",current_score)      
+        player_score[player_index]+=current_score
+        print("Your total score is:",player_score[player_index])        
+
+max_score=max(player_score)
+winning_index=player_score.index(max_score)        
+print("Winner of the game is player",winning_index+1,"with a score of",max_score)
